573ab5069476013a58453ca27b0b311ee6fb4f86
Angus Fraser <angus.fraser@mangahigh.com>
2019-12-01 11:02:05 +0000

    solve part two

--- day_1/index.js
+++ day_1/index.js
@@ -1,4 +1,4 @@
-const fs = require('fs')
+const fs = require('fs');
 
 const input = fs.readFileSync('input.txt', 'utf-8');
 
@@ -6,8 +6,21 @@ const nums = input.split('\n').map(Number);
 
 let total = 0;
 
-nums.forEach(num => {
-    total += Math.floor(num / 3) - 2
+nums.forEach((num) => {
+  total += getFuelRequirement(num);
 });
 
-console.log({total})
+console.log({ total });
+
+function getFuelRequirement(num) {
+  let total = 0;
+  let val = num;
+
+  while (val > 0) {
+    const fuel = Math.floor(val / 3) - 2;
+    total += fuel > 0 ? fuel : 0;
+    val = fuel;
+  }
+
+  return (total);
+}

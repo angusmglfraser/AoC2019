d1728bc83cf9be83cf139275096c6bf63a5d2cef
Angus Fraser <angus.fraser@mangahigh.com>
2019-12-06 09:40:09 +0000

    day 6 part 2

--- day_6/index.js
+++ day_6/index.js
@@ -1,10 +1,6 @@
 const fs = require('fs');
 
-function countToRoot(object, key) {
-    if (!object[key]) return 0;
-    return 1 + countToRoot(object, object[key]);
-}
-const orbits = { com: null };
+const nodes = {};
 
 const input = fs.readFileSync('input.txt', 'utf-8');
 
@@ -13,15 +9,53 @@ const arr = input.split('\n');
 arr.forEach((entry) => {
     const [parent, child] = entry.split(')');
 
-    orbits[child] = parent;
-});
+    if (!nodes[parent]) {
+        nodes[parent] = { neighbours: [child], visited: false, value: Infinity };
+    } else {
+        nodes[parent].neighbours.push(child);
+    }
 
-let checkSum = 0;
-
-Object.keys(orbits).forEach((orbit) => {
-    if (orbits[orbit]) {
-        checkSum += countToRoot(orbits, orbit);
+    if (!nodes[child]) {
+        nodes[child] = { neighbours: [parent], visited: false, value: Infinity };
+    } else {
+        nodes[child].neighbours.push(parent);
     }
 });
 
-console.log({ checkSum });
+nodes.YOU.value = 0;
+nodes.YOU.visited = true;
+
+const nodeSet = Object.values(nodes);
+
+while (nodeSet.length > 0) {
+    // find node with minimum distance
+    let min = Infinity;
+    let currentNode = nodeSet[0];
+    let nodeIndex = 0;
+    nodeSet.forEach((node, index) => {
+        if (node.value < min) {
+            min = node.value;
+            currentNode = node;
+            nodeIndex = index;
+        }
+    });
+
+    nodeSet.splice(nodeIndex, 1);
+
+    if (currentNode.neighbours.find((x) => x === 'SAN')) {
+        // distance is off by one since the path is
+        // supposed to be between orbits rather than planets
+        console.log({ lengthToSanta: currentNode.value - 1 });
+        break;
+    }
+
+    currentNode.neighbours.forEach((n) => {
+        const neighbour = nodes[n];
+        if (neighbour.visited) {
+            neighbour.value = Math.min(neighbour.value, currentNode.value + 1);
+        } else {
+            neighbour.value = currentNode.value + 1;
+            neighbour.visited = true;
+        }
+    });
+}
